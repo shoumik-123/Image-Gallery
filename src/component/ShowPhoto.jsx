305b27e0f6894f2a955d1416444001d9af58501e
shoumik-123 <shabikyeaminshoumik123@gmail.com>
2023-11-04 04:59:23 +0600

    message

--- src/component/ShowPhoto.jsx
+++ src/component/ShowPhoto.jsx
@@ -4,48 +4,70 @@ import "../assets/css/style.css"
 import UploadPhoto from "./UploadPhoto";
 
 function ShowPhoto() {
-    const photo = ['image-1.webp', 'image-2.webp', 'image-3.webp', 'image-4.webp', 'image-5.webp', 'image-6.webp', 'image-7.webp', 'image-8.webp', 'image-9.webp', 'image-10.webp', 'image-11.webp', 'image-11.webp'];
+    const [photo, setPhoto] = useState([
+        'image-1.webp', 'image-2.webp', 'image-3.webp', 'image-4.webp', 'image-5.webp',
+        'image-6.webp', 'image-7.webp', 'image-8.webp', 'image-9.webp', 'image-10.webp', 'image-11.webp'
+    ]);
+
 
     const [checkedCount, setCheckedCount] = useState(0);
-
     const handleCheckboxChange = (id, isChecked) => {
         if (isChecked) {
             setCheckedCount(checkedCount + 1);
+            setCheckedItems([...checkedItems, id]); // Add the item's index to checkedItems
         } else {
             setCheckedCount(checkedCount - 1);
+            setCheckedItems(checkedItems.filter((item) => item !== id)); // Remove the item's index from checkedItems
         }
     };
 
-    const [activePhoto, setActivePhoto] = useState(null); // Store the active photo ID
 
+    const [activePhoto, setActivePhoto] = useState(null); // Store the active photo ID
     const handlePhotoClick = (id) => {
         setActivePhoto(id); // Set the clicked photo as active
     };
+
+    const [checkedItems, setCheckedItems] = useState([]);
+
+    const handleDelete = () => {
+        const updatedPhoto = photo.filter((item, i) => !checkedItems.includes(i));
+        setPhoto(updatedPhoto);
+        setCheckedItems([]); // Clear the selected items
+        setActivePhoto(null);
+        setCheckedCount(0);
+
+
+    };
+
 
     return (
         <>
             <div className='m-3 p-2'>
                 <div className=' row p-3'>
-                    <div className='col-md-3 justify-content-center'>
-                        <h3 className="" style={{ paddingLeft: 20 }}>
+                    <div className='col-md-3'>
+                        <div className="select-checkbox-div" >
+
                             {checkedCount > 0 ? (
-                                <>
-                                    <input className="px-4" type="checkbox" checked />
-                                    &nbsp;&nbsp;{checkedCount}&nbsp;&nbsp;
-                                </>
+                                <p className="text-gray-600 font-semibold">
+                                    {checkedCount} {checkedCount > 1 ? "Files" : "File"} Selected
+                                </p>
                             ) : (
-                                <>
-                                    <input className="px-4" type="checkbox" disabled />
-                                    &nbsp;&nbsp;{checkedCount}&nbsp;&nbsp;
-                                </>
+                                <p> Gallery</p>
                             )}
-                            file selected
-                        </h3>
+                        </div>
 
                     </div>
                     <div className="col-md-6"></div>
                     <div className='col-md-3 delete-div'>
-                        <button className="btn" style={{ color: "red" }}><h4>Delete file</h4></button>
+                        {checkedCount > 0 && (
+                            <button
+                                onClick={handleDelete}
+                                className="btn"
+                            >
+                                Delete {checkedCount > 1 ? "Files" : "File"}
+
+                            </button>
+                        )}
                     </div>
                 </div>
                 <hr />
