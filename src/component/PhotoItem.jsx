fa928f525137c5cb8ae5bad1775754a0a24b326b
shoumik-123 <shabikyeaminshoumik123@gmail.com>
2023-11-03 06:19:08 +0600

    message

--- src/component/PhotoItem.jsx
+++ src/component/PhotoItem.jsx
@@ -1,29 +1,50 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-function PhotoItem({ name, id ,handleCheckboxChange }) {
+function PhotoItem({ name, id, handleCheckboxChange, active, onClick }) {
     const [isChecked, setIsChecked] = useState(false);
+    const [scaleActive, setScaleActive] = useState(false);
+
     const toggleCheckbox = () => {
         setIsChecked(!isChecked);
         handleCheckboxChange(id, !isChecked);
     };
 
+    const photoStyle = {
+        zIndex: active ? 1000 : 0,
+        height: active ? '100%' : '80%',
+        width: active ? '100%' : '80%',
+        transform: active ? 'scale(1.5)' : 'scale(1)',
+        marginRight: active? 0 :20,
+        padding: active? 0 :20,
+    };
+
+    const handleMouseEnter = () => {
+        setScaleActive(true);
+    };
+
+    const handleMouseLeave = () => {
+        setScaleActive(false);
+    };
+
+    const handlePhotoClick = () => {
+        onClick(id);
+        setScaleActive(!scaleActive);
+    };
 
     return (
-        <div className="">
-            {id === "" ? (
-                <>
-                    <input type="checkbox" onChange={() => handleCheckboxChange(id)} className="form-check-input ml-2 mt-2" />
-                    <img src={process.env.PUBLIC_URL + `/images/${name}`} alt="" className="" style={{ width: 800 , height:800}} />
-                </>
-            ) : (
-                <>
-                    <input type="checkbox" checked={isChecked} onChange={toggleCheckbox} className="form-check-input ml-2 mt-2" />
-                    <img src={process.env.PUBLIC_URL + `/images/${name}`} alt="" className="" style={{ width: 400 , height:400}} />
-                </>
-            )}
+        <div className="card">
+            <input type="checkbox" checked={isChecked} onChange={toggleCheckbox} className="form-check-input ml-2 mt-2" />
+            <img
+                src={process.env.PUBLIC_URL + `/images/${name}`}
+                alt=""
+                className={`photo ${active ? 'active-photo' : 'photo'}`}
+                onClick={() => { onClick(); handlePhotoClick(); }}
+                style={photoStyle}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}/>
         </div>
-
     );
 }
 
 export default PhotoItem;
+
--- src/component/ShowPhoto.jsx
+++ src/component/ShowPhoto.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import PhotoItem from "./PhotoItem";
+import "../assets/css/style.css"
 
 function ShowPhoto() {
     const photo = ['image-1.webp', 'image-2.webp', 'image-3.webp', 'image-4.webp', 'image-5.webp', 'image-6.webp', 'image-7.webp', 'image-8.webp', 'image-9.webp', 'image-10.webp', 'image-11.webp', 'image-11.webp'];
@@ -14,27 +15,52 @@ function ShowPhoto() {
         }
     };
 
+    const [activePhoto, setActivePhoto] = useState(null); // Store the active photo ID
+
+    const handlePhotoClick = (id) => {
+        setActivePhoto(id); // Set the clicked photo as active
+    };
+
     return (
         <>
-            <div className='m-3 p-2' style={{ boxSizing: "border-box" }}>
+            <div className='m-3 p-2'>
                 <div className=' row p-3'>
                     <div className='col-md-3 justify-content-center'>
                         <h3 className="" style={{ paddingLeft: 20 }}>
-                            {checkedCount} file selected
+                            {checkedCount > 0 ? (
+                                <>
+                                    <input className="px-4" type="checkbox" checked />
+                                    &nbsp;&nbsp;{checkedCount}&nbsp;&nbsp;
+                                </>
+                            ) : (
+                                <>
+                                    <input className="px-4" type="checkbox" disabled />
+                                    &nbsp;&nbsp;{checkedCount}&nbsp;&nbsp;
+                                </>
+                            )}
+                            file selected
                         </h3>
+
                     </div>
                     <div className="col-md-6"></div>
-                    <div className='col-md-3 ' style={{ textAlign: 'right', paddingRight: 20 }}>
+                    <div className='col-md-3 delete-div'>
                         <button className="btn" style={{ color: "red" }}><h4>Delete file</h4></button>
                     </div>
                 </div>
                 <hr />
+                <br/>
                 <div className="container-fluid p-5">
                     <div className="row">
-                        {photo.map((name, index) => (
-                            <div key={index} className="col-md-2 mb-3">
-                                <div className="card" style={{ width: '400' }}>
-                                    <PhotoItem id={index} name={name} handleCheckboxChange={handleCheckboxChange} />
+                        {photo.map((item, i) => (
+                            <div key={i} className="col-md-2 mb-3 photo">
+                                <div className="">
+                                    <PhotoItem
+                                        id={i}
+                                        name={item}
+                                        active={activePhoto === i}
+                                        onClick={() => handlePhotoClick(i)}
+                                        handleCheckboxChange={handleCheckboxChange}
+                                    />
                                 </div>
                             </div>
                         ))}
